6c7ee74e68c4a79cae2180b843da5d011f1eefdd
Bogdan Tancic <bogdanzemunska@gmail.com>
2026-01-22 15:17:50 +0100

    app.jsx i main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,40 @@
-import './App.css'
-
+import ChatForma from './components/ChatForma'
+import LoginForma from './components/LoginForma'
+import Profile from './components/Profile'
+import ProtectedRoute from './components/ProtectedRoute'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react"
 
 function App() {
+  const [isRegistering, setIsRegistering] = useState(false) // State za prebacivanje između login i register
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className="bg-yellow-200 h-screen flex items-center justify-center">
+      <Routes>
+
+        <Route path="/" element={
+          <LoginForma isRegistering={isRegistering} setIsRegistering={setIsRegistering} />}
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <ChatForma />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </main>
   )
 }
 
